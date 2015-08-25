e26552ab10d0ffa39faab4fe25ed877904e9f775
Neil Gewirtz <ngewirtz86@gmail.com>
2015-08-25 19:25:25 -0400

    search working

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :bands
   resources :events
   resources :concert_halls
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
